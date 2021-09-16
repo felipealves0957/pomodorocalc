269673e71396e805072034dfe0e97c43f0d1b09e
Felipe Alves <felipealves0957@gmail.com>
2021-09-16 11:01:27 -0300

    Padronizaçao na nomeaçao de variaveis

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def minutes_to_hours(min):
     minutes = min - hours*60
     return (trunc(hours), trunc(minutes))
 
-def make_lines(nlines, mold):
+def make_lines(mold, nlines):
     print(f'{mold}'*nlines)
 
 def format_time(time, ishour = False):
@@ -21,16 +21,13 @@ def format_time(time, ishour = False):
 
 from math import trunc
 from time import sleep
-hour = 0
-minutes = 0
-line_mold = '/'
+hour = minutes = 0
+line_mold = '~'
 nlines = 60
 
-make_lines(nlines, line_mold)
-print('Olá...'.center(nlines))
-sleep(1.5)
+make_lines(line_mold, nlines)
 print('Seja bem-vindo(a) ao PomodoroPlanner'.center(nlines))
-make_lines(nlines, line_mold)
+make_lines(line_mold, nlines)
 
 start_time = str(input('Horário de início (hh:mm): '))
 pomodoro_time = int(input('Duração de cada pomodoro (minutos): '))
@@ -40,17 +37,20 @@ rest_time = int(input('Tempo de descanso (minutos): '))
 hour = start_time[:2]
 minutes = start_time[3:]
 
-rest_total = ((total_minutes/pomodoro_time) - 1)*rest_time
-total_time = rest_total+total_minutes+int(minutes)
+total_rest = ((total_minutes/pomodoro_time) - 1)*rest_time
+total_time = total_rest+total_minutes+int(minutes)
 
 final_hour = int(hour)+minutes_to_hours(total_time)[0]
 final_minutes = minutes_to_hours(total_time)[1]
 
-make_lines(nlines, line_mold)
+make_lines(line_mold, nlines)
 print(f'Horário inicial: {hour}:{minutes}'.center(nlines))
 print(f'Horário final: {format_time(final_hour, True)[0]}:{format_time(final_minutes)[0]}'.center(nlines))
 
-if format_time(final_hour, True)[1] > 0:
-    print(f'{format_time(final_hour, True)[1]} dia(s) depois da data inicial.'.center(nlines))
+day_after_initial_date = format_time(final_hour, True)[1]
+if day_after_initial_date == 1:
+    print(f'1 dia depois do dia inicial.'.center(nlines))
+elif day_after_initial_date > 1: 
+    print(f'{day_after_initial_date} dias depois do dia inicial.'.center(nlines))
 
-make_lines(nlines, line_mold)
+make_lines(line_mold, nlines)
